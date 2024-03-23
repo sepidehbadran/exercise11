d4527986ae8ccd902dcd781041bf7208da3c5589
Sepideh <sepideh.badran05@gmail.com>
2024-03-23 21:31:54 +0330

    exersice11

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("please the investment amount :");
+        double initia1investment = double.Parse(Console.ReadLine());
+        double annua1interestRate = 45.0 / 1000.0;
+        double tota1investment = initia1investment;
+        for (int i =1; i <= 10; i++)
+        {
+            double annua1Profit = tota1investment * annua1interestRate;
+            tota1investment += annua1Profit;
+            Console.WriteLine("capital after the year" + i + ":" + tota1investment);
+            Console.WriteLine(" the final capital after eight years:" + tota1investment);
+        }
+    }
+}
